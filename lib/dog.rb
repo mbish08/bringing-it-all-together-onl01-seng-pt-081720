8ed057e698c752864203193788171f27d4d745e6
Mikel Bishop <mdrew8@gmail.com>
2020-11-05 02:19:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,15 +52,18 @@ class Dog
     self.new(new_dog)
   end 
   
-  
-  # def self.new_from_db(row)
-  #   attributes_hash = {
-  #     :id => row[0],
-  #     :name => row[1],
-  #     :breed => row[2]
-  #   }
-  #   self.new(attributes_hash)
-  # end
+  def self.find_by_id(id)
+    sql = <<-SQL
+    SELECT *
+    FROM dogs
+    WHERE id = ?
+    LIMIT 1 
+    SQL
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first 
+  end 
+ 
   
   
   
