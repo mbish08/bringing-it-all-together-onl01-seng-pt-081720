cac7a47ab5813c89e45a6a89fdb439fd32c2f67d
Mikel Bishop <mdrew8@gmail.com>
2020-11-05 02:42:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -86,14 +86,32 @@ class Dog
    dog = DB[:conn].execute(sql, name, breed).first 
    
    if dog 
-     new_dog = self.new_from_db
+     new_dog = Dog.new(id: dog[0], name: dog[1], breed: dog[2])
    else 
      new_dog = self.create({:name => name, :breed => breed})
    end 
    new_dog
  end 
   
-  
+  # def self.find_or_create_by(name:, breed:)
+  #   sql = <<-SQL
+  #         SELECT *
+  #         FROM dogs
+  #         WHERE name = ?
+  #         AND breed = ?
+  #         LIMIT 1
+  #       SQL
+
+  #   dog = DB[:conn].execute(sql,name,breed)
+
+  #   if !dog.empty?
+  #     dog_data = dog[0]
+  #     dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+  #   else
+  #     dog = self.create(name: name, breed: breed)
+  #   end
+  #   dog
+  # end
   
   
 end 
