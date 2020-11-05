1169d34d3f2fa9b97a4fc8811466f40996728c9f
Mikel Bishop <mdrew8@gmail.com>
2020-11-05 01:58:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,6 +11,11 @@ class Dog
   
   def self.create_table
     sql = <<-SQL 
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+      )
   end 
   
   
