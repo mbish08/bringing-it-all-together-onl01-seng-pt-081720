0f200bf3a065bc98d3aa2728172bfeb9be17be55
Mikel Bishop <mdrew8@gmail.com>
2020-11-05 02:04:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,7 +27,14 @@ class Dog
     DB[:conn].execute(sql)
   end 
   
-  
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs(name, breed)
+      VALUES(?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.breed)
+    
+  end 
   
   
   
